71c45a75712ea18869bbe4bfb0895f8c783d7dca
wcb <897927098@qq.com>
2021-04-14 17:25:29 +0800

    util包下ABC类第六行添加了master

--- src/main/java/com/atguigu/util/ABC.java
+++ src/main/java/com/atguigu/util/ABC.java
@@ -3,6 +3,6 @@ package com.atguigu.util;
 public class ABC {
 
     public static void main(String[] args) {
-        System.out.println("111111111111");
+        System.out.println("111111111111master");
     }
 }
